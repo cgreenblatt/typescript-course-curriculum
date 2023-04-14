7b9e9df460537aee992a856b6f8c58ca4c89fafd
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-04-13 18:25:58 -0700

    fix: Implement User component

--- src/components/User.ts
+++ src/components/User.ts
@@ -1,8 +1,42 @@
 import { UserResponseT } from '../models/index';
+import { UserT } from '../models/api';
+import { formatDate } from '../utils/helpers';
+import Posts from './Posts';
 
-export default function User(user: UserResponseT): HTMLElement {
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const userHeader = document.createElement('h1');
-  userHeader.textContent = user.user.id;
-  return userHeader;
+function userMetaInfo(user: UserT) {
+  const div = document.createElement('div');
+  div.className = 'meta-info-light';
+  const joinedSpan = document.createElement('span');
+  joinedSpan.className = 'bold';
+  joinedSpan.textContent = formatDate(user.created);
+  div.appendChild(joinedSpan);
+  div.appendChild(document.createTextNode('has '));
+  if (user.karma) {
+    const karmaSpan = document.createElement('span');
+    karmaSpan.className = 'bold';
+    karmaSpan.textContent = user.karma.toLocaleString();
+    div.appendChild(karmaSpan);
+    div.appendChild(document.createTextNode('karma'));
+  }
+  return div;
+}
+
+export default function User(userData: UserResponseT): HTMLElement {
+  const { user, posts } = userData;
+  const div = document.createElement('div');
+  const h1 = document.createElement('h1');
+  h1.className = 'header';
+  h1.textContent = user.id;
+  div.appendChild(h1);
+  div.appendChild(userMetaInfo(user));
+  if (user.about) {
+    const p = document.createElement('p');
+    p.innerHTML = user.about;
+    div.appendChild(p);
+  }
+  const h2 = document.createElement('h2');
+  h2.textContent = 'Posts';
+  div.appendChild(h2);
+  div.appendChild(Posts(posts));
+  return div;
 }
